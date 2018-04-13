1a574e5b0c9ea1cf3a661c01a1c0c08f64d30887
Your Name <wowbob396@gmail.com>
2018-04-12 19:49:01 -0500

    added examples

---
+++ examples/tutorial.rs
@@ -0,0 +1,12 @@
+extern crate adi_screen;
+
+use adi_screen:: {
+    Window, Input, Transform, Texture, Model, TexCoords, Key, 
+    SpriteList, Text, DEFAULT_FONT
+};
+
+use std::f32::consts::PI;
+
+fn main() {
+    let mut window = Window::new("Physics Test", &aci_png::decode());
+}

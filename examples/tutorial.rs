6bb7b231fdf45020ddd448c82291b27ef0780a8f
Jeron A. Lau <jeron.lau@plopgrizzly.com>
2018-04-12 19:55:46 -0500

    Added window icon.

--- examples/tutorial.rs
+++ examples/tutorial.rs
@@ -1,12 +1,16 @@
 extern crate adi_screen;
 
 use adi_screen:: {
-    Window, Input, Transform, Texture, Model, TexCoords, Key, 
-    SpriteList, Text, DEFAULT_FONT
+	Window, Input, Transform, Texture, Model, TexCoords, Key, 
+	SpriteList, Text, DEFAULT_FONT
 };
 
 use std::f32::consts::PI;
 
 fn main() {
-    let mut window = Window::new("Physics Test", &aci_png::decode());
+	let mut window = Window::new("Physics Test", &aci_png::decode(
+		include_bytes!("res/plopgrizzly.png")).unwrap(),
+		(0.25, 0.25, 1.0), None);
+
+	
 }

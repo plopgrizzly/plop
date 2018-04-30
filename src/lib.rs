3407d771d011ac052bb3c77e488d6f79882d62ec
Your Name <wowbob396@gmail.com>
2018-04-30 14:17:08 -0500

    added basic structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate adi_screen;
+use adi_screen::Sprite;
+
+const GRAVITY:f32 = 9.81;
+const METER:f32 = 1.0;
+
+pub struct RigidBody {
+    mass:f32,
+    x:f32,
+    y:f32,
+    z:f32,
+    sprite:Sprite,
+}
+
+pub fn force(object: RigidBody) {
+
 }
